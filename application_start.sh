10e22a5cf95c8943745171c30509c3b37cbdebc4
Raj Kumar <raj00565@gmail.com>
2021-01-21 10:28:41 +0530

    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -14,7 +14,6 @@ if [ $? != 0 ]; then
     pip3 install ansible
 fi
 
-service puma restart &> /dev/null
-service sidekiq restart &> /dev/null
+
 
 #ansible-playbook $DESTINATION_PATH/AnsibleScripts/site.yml -i $DESTINATION_PATH/AnsibleScripts/hosts --connection=local 
